f6119d15620389d3e8021936c5d9dc7971937069
Giovana Proto <giovana.proto@gmail.com>
2025-11-18 17:08:06 -0300

    todos os arquivos do projeto

---
+++ src/app/modules/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'home-page',
+    templateUrl: 'home.page.html',
+    styleUrl: 'home.page.scss'
+})
+
+export class HomePage
+{
+
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { HomePage } from './app/modules/home/home.page';
+
+bootstrapApplication(HomePage, appConfig)
+  .catch((err) => console.error(err));
