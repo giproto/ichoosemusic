bc504f446bd69f82c97a3419011d30a8692073b9
Giovana Proto <giovana.proto@gmail.com>
2025-11-21 12:30:47 -0300

    atualização de alterações no projeto

--- src/app/modules/home/home.page.ts
+++ src/app/modules/home/home.page.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { IMusicCard } from "../../shared/components/imusic-card/imusic-card.component";
 
 @Component({
     selector: 'home-page',
     templateUrl: 'home.page.html',
-    styleUrl: 'home.page.scss'
+    styleUrl: 'home.page.scss',
+    imports: [CommonModule, FontAwesomeModule, IMusicCard]
 })
 
 export class HomePage
 {
-
+    public iconSearch = faMagnifyingGlass;
 }
---
+++ src/app/shared/components/imusic-card/imusic-card.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'imusic-card',
+    templateUrl: 'imusic-card.component.html',
+    styleUrl: 'imusic-card.component.scss',
+    imports: [CommonModule]
+})
+
+export class IMusicCard {}
